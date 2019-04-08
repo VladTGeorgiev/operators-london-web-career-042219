f4cef8fa06d5f1ca487bfb6102d9663d6fa73009
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-08 17:32:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   if speed > 60 || speed <40
-    "t"
+    true
   else
-    "f"
+    false
 end
 
 def not_safe?(speed)
-  speed > 60 || speed <40? true:false
+  speed > 60 || speed <40? true : false
 end
